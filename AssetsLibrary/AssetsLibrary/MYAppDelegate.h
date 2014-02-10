8e69281a55e6fccc0e4877b96f86396ec2f1a5d8
PGMY <pgmyuri@gmail.com>
2014-02-10 18:06:43 +0900

    add new xcode project

---
+++ AssetsLibrary/AssetsLibrary/MYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MYAppDelegate.h
+//  AssetsLibrary
+//
+//  Created by Mika Yamamoto on 2014/02/10.
+//  Copyright (c) 2014年 MikaYamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AssetsLibrary/AssetsLibrary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AssetsLibrary
+//
+//  Created by Mika Yamamoto on 2014/02/10.
+//  Copyright (c) 2014年 MikaYamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MYAppDelegate class]));
+    }
+}
